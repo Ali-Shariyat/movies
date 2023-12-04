23803d0ba843bfcb2a803fa01d1b64dcdbc7ec9a
Ali Shariyat <a.shariyat.78.99.20@gmail.com>
2023-12-05 00:25:14 +0330

    edit proxy mode

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
 export const apiConfig = {
-    api: import.meta.env.VITE_API + import.meta.env.VITE_API_PREFIX,
-    apiSecond: import.meta.env.VITE_API_SECOND + import.meta.env.VITE_API_SECOND_PREFIX,
+    api: import.meta.env.VITE_API,
+    apiSecond: import.meta.env.VITE_API_SECOND,
 }
